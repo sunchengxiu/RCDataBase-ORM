f7c03fb4ae10c94dd0478aeb8ed6fcc2bef7512d
孙承秀 <>
2018-10-11 10:47:18 +0800

    workspace 创建

---
+++ RCDataBase/RCDataBase/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RCDataBase
+//
+//  Created by 孙承秀 on 2018/10/11.
+//  Copyright © 2018年 RongCloud. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ RCDataBase/RCDataBaseTests/RCDataBaseTests.m
@@ -0,0 +1,37 @@
+//
+//  RCDataBaseTests.m
+//  RCDataBaseTests
+//
+//  Created by 孙承秀 on 2018/10/11.
+//  Copyright © 2018年 RongCloud. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RCDataBaseTests : XCTestCase
+
+@end
+
+@implementation RCDataBaseTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
